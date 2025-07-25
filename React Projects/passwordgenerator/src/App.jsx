b7570fbe9e348374ca7584a4803aec0ca940e31c
SudeepPattanshetty <sudeeppattanshetty161@gmail.com>
2025-07-25 17:54:37 +0530

    created React Project

---
+++ React Projects/passwordgenerator/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8">
+        <h1 className="text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+          />
+          <button className="outline-none py-0.5 px-3 text-white bg-blue-700 shrink-0">
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <label htmlFor="length">Length: {length}</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
